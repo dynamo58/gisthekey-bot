d502f3c2eee0844b029aa75c09676b0fc21fffa2
dynamo58 <sailorfirex@seznam.cz>
2022-03-06 12:40:01 +0100

    some revamps okge

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 STREAMER = "Gisthekey"		# streamer to keep track
-PERIOD = 40					# delay between streamer status checks 
+PERIOD = 40					# delay between streamer status checks
+	# this could be made way better via EventSub, but cba
+	# maybe some time
 
 # Do not change anything below, unless you know what you're doing!
 # ... that is, stepping away from CLI flags and hardcoding the config
